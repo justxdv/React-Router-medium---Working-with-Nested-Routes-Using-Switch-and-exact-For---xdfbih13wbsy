2a7aa079a68fd3b67d2457b557d23566cb1f4e4a
Digvijay Sethi <59555227+justxdv@users.noreply.github.com>
2023-02-23 15:43:19 +0530

    Update Calculator.js

--- src/Pages/Calculator.js
+++ src/Pages/Calculator.js
@@ -2,8 +2,34 @@ import React from "react";
 
 
 export const Calculator = () =>{
+const params = new URLSearchParams(window.location.search);
+  const num1 = parseInt(params.get('num1'));
+  const num2 = parseInt(params.get('num2'));
+  const op = decodeURIComponent(params.get('op'));
+    let result;
+  switch (op) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    case '/':
+      result = num1 / num2;
+      break;
+    case '%':
+      result = num1 % num2;
+      break;
+    default:
+      result = NaN;
+  }
 
-    return(
-        
-    )
+  return (
+    <div>
+      <h2>Your calculation result is <span id="calc-result">{result}</span></h2>
+    </div>
+  );
 }
